ec1fad066b2559e267abf9a5dded08fdd7048c25
ROBIN FANTINO <robin.fantino.rf@gmail.com>
2023-11-28 10:56:00 +0100

    commit

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,23 +1,24 @@
 const database = require("../../database");
 
-const getMovies = (req, res) => {
+const getUser = (req, res) => {
   database
-    .query("select * from movies")
-    .then(([movies]) => {
-      res.json(movies); // use res.json instead of console.log
+    .query("SELECT * FROM users")
+    .then(([users]) => {
+      res.json(users);
     })
     .catch((err) => {
       console.error(err);
       res.sendStatus(500);
     });
 };
-const getMovieById = (req, res) => {
+
+const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
   database
-    .query("select * from movies where id = ?", [id])
-    .then(([movies]) => {
-      if (movies[0] != null) {
-        res.json(movies[0]);
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.status(200).json(users[0]);
       } else {
         res.sendStatus(404);
       }
@@ -29,6 +30,6 @@ const getMovieById = (req, res) => {
 };
 
 module.exports = {
-  getMovies,
-  getMovieById,
+  getUser,
+  getUserById,
 };
