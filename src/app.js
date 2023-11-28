ecd633586f5e2216ef64c4604339ea502b8fc199
ROBIN FANTINO <robin.fantino.rf@gmail.com>
2023-11-28 12:22:32 +0100

    chanllenge 2.0 quest ok

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
 const userControllers = require("./controllers/userControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.post("/api/movies", movieControllers.postMovie);
 app.get("/api/users", userControllers.getUser);
 app.get("/api/users/:id", userControllers.getUserById);
+app.post("/api/users", userControllers.postUser);
 
 module.exports = app;
